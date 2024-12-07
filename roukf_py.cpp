12b8566411a8ca3042d23468842864e1ae04d7b7
kekayan <kekayannanthakumar@gmail.com>
2024-12-07 20:07:45 +1300

    debug

--- roukf_py.cpp
+++ roukf_py.cpp
@@ -27,19 +27,16 @@ py::function CallbackStorage::observation_func;
 // Safe wrapper functions
 int forward_wrapper(double* states, int n_states, double* params, int n_params) {
     py::gil_scoped_acquire gil;
-    std::cerr << "Entering forward_wrapper" << std::endl;
     
     try {
         auto states_array = py::array_t<double>(n_states, states);
         auto params_array = py::array_t<double>(n_params, params);
         
-        std::cerr << "Calling Python forward function" << std::endl;
         py::object result = CallbackStorage::forward_func(states_array, n_states, params_array, n_params);
         
         std::memcpy(states, states_array.data(), n_states * sizeof(double));
         std::memcpy(params, params_array.data(), n_params * sizeof(double));
         
-        std::cerr << "Exiting forward_wrapper" << std::endl;
         return result.cast<int>();
     } catch (const std::exception& e) {
         std::cerr << "Forward wrapper error: " << e.what() << std::endl;
@@ -49,19 +46,16 @@ int forward_wrapper(double* states, int n_states, double* params, int n_params) 
 
 void observation_wrapper(double* states, int n_states, double* obs, int n_obs) {
     py::gil_scoped_acquire gil;
-    std::cerr << "Entering observation_wrapper" << std::endl;
     
     try {
         auto states_array = py::array_t<double>(n_states, states);
         auto obs_array = py::array_t<double>(n_obs, obs);
         
-        std::cerr << "Calling Python observation function" << std::endl;
         CallbackStorage::observation_func(states_array, n_states, obs_array, n_obs);
         
         std::memcpy(states, states_array.data(), n_states * sizeof(double));
         std::memcpy(obs, obs_array.data(), n_obs * sizeof(double));
         
-        std::cerr << "Exiting observation_wrapper" << std::endl;
     } catch (const std::exception& e) {
         std::cerr << "Observation wrapper error: " << e.what() << std::endl;
     }
@@ -82,24 +76,18 @@ PYBIND11_MODULE(roukf_py, m) {
     py::class_<AbstractROUKF>(m, "AbstractROUKF")
         .def(py::init<>())
         .def("getState", [](AbstractROUKF& self) {
-            std::cerr << "Entering getState" << std::endl;
             double* state;
             self.getState(&state);
             py::array_t<double> result(self.getStates());
             std::memcpy(result.mutable_data(), state, self.getStates() * sizeof(double));
-            std::cerr << "Exiting getState" << std::endl;
             return result;
         })
         .def("setState", [](AbstractROUKF& self, py::array_t<double> array) {
-            std::cerr << "Entering setState" << std::endl;
             self.setState(array.mutable_data());
-            std::cerr << "Exiting setState" << std::endl;
         })
         .def("getError", [](AbstractROUKF& self) {
-            std::cerr << "Entering getError" << std::endl;
             double* error;
             self.getError(&error);
-            std::cerr << "Exiting getError" << std::endl;
             return error;
         })
         .def("getObsError", &AbstractROUKF::getObsError)
@@ -111,9 +99,7 @@ PYBIND11_MODULE(roukf_py, m) {
         .def("setMaxIterations", &AbstractROUKF::setMaxIterations)
         .def("hasConverged", &AbstractROUKF::hasConverged)
         .def("setParameters", [](AbstractROUKF& self, py::array_t<double> array) {
-            std::cerr << "Entering setParameters" << std::endl;
             self.setParameters(array.mutable_data());
-            std::cerr << "Exiting setParameters" << std::endl;
         });
 
     // Bind the derived class with corrected reset method
@@ -122,18 +108,15 @@ PYBIND11_MODULE(roukf_py, m) {
                          py::array_t<double> states_uncertainty,
                          py::array_t<double> parameters_uncertainty,
                          SigmaPointsGenerator::SIGMA_DISTRIBUTION sigma_distribution) {
-            std::cerr << "Entering ROUKF constructor" << std::endl;
-            auto instance = new ROUKF(n_observations, n_states, n_parameters,
-                                      states_uncertainty.mutable_data(),
-                                      parameters_uncertainty.mutable_data(),
-                                      sigma_distribution);
-            std::cerr << "Exiting ROUKF constructor" << std::endl;
-            return instance;
+            return new ROUKF(n_observations, n_states, n_parameters,
+                             states_uncertainty.mutable_data(),
+                             parameters_uncertainty.mutable_data(),
+                             sigma_distribution);
         }))
         .def("executeStep", [](ROUKF& self, py::array_t<double> observations,
                                py::function forward_func,
                                py::function observation_func) {
-            std::cerr << "Entering executeStep" << std::endl;
+            std::cout << "executeStep" << std::endl;
             py::gil_scoped_release release;
             
             py::buffer_info obs_buf = observations.request();
@@ -144,29 +127,23 @@ PYBIND11_MODULE(roukf_py, m) {
                 CallbackStorage::setCallbacks(forward_func, observation_func);
             }
             
-            auto result = self.executeStep(
+            return self.executeStep(
                 static_cast<double*>(obs_buf.ptr),
                 forward_wrapper,
                 observation_wrapper
             );
-            std::cerr << "Exiting executeStep" << std::endl;
-            return result;
-        }, "Execute a single step of the ROUKF algorithm",
-           py::arg("observations"),
-           py::arg("forward_func"),
-           py::arg("observation_func"))
+        })
         .def("executeStepParallel", [](ROUKF& self, py::array_t<double> observations,
                                       std::function<int(double*, int, double*, int)> forward_func,
                                       std::function<void(double*, int, double*, int)> observation_func,
                                       int seed) {
-            std::cerr << "Entering executeStepParallel" << std::endl;
             py::buffer_info obs_buf = observations.request();
             auto forward_ptr = forward_func.target<int(*)(double*, int, double*, int)>();
             auto observation_ptr = observation_func.target<void(*)(double*, int, double*, int)>();
             if (!forward_ptr || !observation_ptr) {
                 throw std::runtime_error("Function pointers cannot be null");
             }
-            auto result = self.executeStepParallel(
+            return self.executeStepParallel(
                 static_cast<double*>(obs_buf.ptr),
                 reinterpret_cast<forwardOp>(*forward_ptr),
                 reinterpret_cast<observationOp>(*observation_ptr),
@@ -174,14 +151,11 @@ PYBIND11_MODULE(roukf_py, m) {
                 MPI_COMM_WORLD,
                 MPI_COMM_WORLD
             );
-            std::cerr << "Exiting executeStepParallel" << std::endl;
-            return result;
         })
         .def("reset", [](ROUKF& self, int n_observations, int n_states, int n_parameters,
                          py::array_t<double> states_uncertainty,
                          py::array_t<double> parameters_uncertainty,
                          SigmaPointsGenerator::SIGMA_DISTRIBUTION sigma_distribution) {
-            std::cerr << "Entering reset" << std::endl;
             py::buffer_info states_buf = states_uncertainty.request();
             py::buffer_info params_buf = parameters_uncertainty.request();
 
@@ -189,6 +163,5 @@ PYBIND11_MODULE(roukf_py, m) {
                        states_uncertainty.mutable_data(),
                        parameters_uncertainty.mutable_data(),
                        sigma_distribution);
-            std::cerr << "Exiting reset" << std::endl;
         });
 }
